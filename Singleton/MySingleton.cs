03114f1c947e2b7bc99295a8865398e9f24c50a6
Ling Wang <44144779+lwangbarfoot@users.noreply.github.com>
2023-12-14 21:16:34 +1300

    Singleton

---
+++ Singleton/MySingleton.cs
@@ -0,0 +1,20 @@
+﻿namespace Singleton;
+
+public static class MySingleton
+{
+    public static string? Instance { get; set; } = "Instance" + GetRandomNumber();
+
+    //static helper method generating random number from 0 to 1000
+    public static int GetRandomNumber()
+    {
+        Random random = new();
+        return random.Next(0, 1000);
+    }
+}
+
+
+public class MySingleton2
+{
+    public object? Instance { get; set; }
+    public static object? Instance2 { get; set; }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+//Singleton is when you only want one instance of a class to exist in your program.
+
+
+
+int n = 45543567;
+var x = new object();
+var y = new object();
+
+
+var s = MySingleton.Instance;
+var s2 = MySingleton.Instance;
+
+MySingleton2.Instance2 = new();
+var s3 = new MySingleton2();
+s3.Instance = new();
+
+// two types of memory
+// stack
+// 0001: 45543567
+// 0002: h0001
+// 0003: h0003
+// 0004: h0005
+// 0005: h0005
+
+// reference type: class
+// heap
+// 0001: x-begin
+// 0002: x-end
+// 0003: y-begin
+// 0004: y-end
+// 0005: s-begin
+// 0006: s-end
